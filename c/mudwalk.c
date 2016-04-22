b011987e32e7f30c447a1e1557b9ea5b2074bde9
pat miller <pat@patmillersax.com>
2016-04-21 23:22:42 -0700

    fixing indentation

--- c/mudwalk.c
+++ c/mudwalk.c
@@ -29,152 +29,152 @@ int REVERSE=0; /* new feature as of 0.11 reverse flag allows for directions to o
 
 
 int strintadec() {
-/*must convert the character type to an integer, so convert the character to a string, then to an int
-explanation of variables:
-*/
-char TEMPSTR[1]; /*local var to hold character value for conversion*/
-int E; /*we shall use this as the integer value of the newly converted digit*/
-TEMPSTR[0]=C;
-E=atoi(TEMPSTR);
-/*printf("%d\n",E);*/
-return E;
+	/*must convert the character type to an integer, so convert the character to a string, then to an int
+	explanation of variables:
+	*/
+	char TEMPSTR[1]; /*local var to hold character value for conversion*/
+	int E; /*we shall use this as the integer value of the newly converted digit*/
+	TEMPSTR[0]=C;
+	E=atoi(TEMPSTR);
+	/*printf("%d\n",E);*/
+	return E;
 }
 
 
 int numbers(){
-int F;/*using F to receive str to int value for current digit position*/
-F=strintadec();
+	int F;/*using F to receive str to int value for current digit position*/
+	F=strintadec();
 
-if ( D > 0 ) /*shift the ten's place to make way for the next digit in the series*/
-	D = ( D * 10 + F );
+	if ( D > 0 ) /*shift the ten's place to make way for the next digit in the series*/
+		D = ( D * 10 + F );
 	else D = F; /*or if first digit, assign digit that value*/
 }
 
 
 int directions(){
-/*directions takes individual direction characters and adds them to the end of the new string. */
-if ( D > 0 ) {
-	/*the following loop deals with directional characters that have been preceded by digits.
-	It adds D amount of directional characters of the type discovered */
-	for ( X=1; X<=D; X++ ){ 
+	/*directions takes individual direction characters and adds them to the end of the new string. */
+	if ( D > 0 ) {
+		/*the following loop deals with directional characters that have been preceded by digits.
+		It adds D amount of directional characters of the type discovered */
+		for ( X=1; X<=D; X++ ){ 
 				
-		NEWSTR[NPOS]=C;
-		NPOS++;
+			NEWSTR[NPOS]=C;
+			NPOS++;
 		}
-	D=0;
+		D=0;
 	}
 	
-/*the "else" is for a lone character without a preceding digit*/
+	/*the "else" is for a lone character without a preceding digit*/
 	else {
-	NEWSTR[NPOS]=C;
-	NPOS++;
+		NEWSTR[NPOS]=C;
+		NPOS++;
 	}
 }
 
 
 print_usage() {
-printf("mudwalk version 0.11 pat@snarg.com\n");
-printf("usage: mudwalk <directions>\n");
-printf("\n");
-printf("for example this command:\n");
-printf(">mudwalk 3n2s5u\n");
-printf("\n");
-printf("should return the following output:\n");
-printf(">mudwalk 3n2s5u\n");
-printf("nnnssuuuuu\n");
-printf(">mudwalk -r 3n2s5u will reverse the output:\n");
-printf(">reversed dddddnnsss\n");
+	printf("mudwalk version 0.11 pat@snarg.com\n");
+	printf("usage: mudwalk <directions>\n");
+	printf("\n");
+	printf("for example this command:\n");
+	printf(">mudwalk 3n2s5u\n");
+	printf("\n");
+	printf("should return the following output:\n");
+	printf(">mudwalk 3n2s5u\n");
+	printf("nnnssuuuuu\n");
+	printf(">mudwalk -r 3n2s5u will reverse the output:\n");
+	printf(">reversed dddddnnsss\n");
 }
 
 reverse() {
-/* reverse does two things:
-1. reverse the stream of characters
-2. reverse each character to it's directional opposite.
-This obviously won't work for asymetrical mud paths, but there's
-not much we can do about that :P
-*/
-int POS1=0; /*position in stream of string of directions 
-that have already been through the numbers() function */
-int STRLENGTH=0; /*length of the string. */
-char P; /* individual character to process */
-char T; /* reversed character */
-STRLENGTH=strlen(NEWSTR); /*get string length*/
-for ( POS1=STRLENGTH; POS1 > 0; POS1-- ) /*create a loop starting at the end of the string and go backwards*/
-	{
-	switch (NEWSTR[POS1-1]) { /* reverse each directional character using a switch */
-           case 'n':
-                T='s';
-                break;
-           case 's':
-                T='n';
-                break;
-           case 'w':
-                T='e';
-                break;
-           case 'e':
-                T='w';
-                break;
-           case 'u':
-                T='d';
-                break;
-           case 'd':
-                T='u';
-                break;
-           default: /* reset the character to null to avoid any possibility of unwanted characters */
-                T='\0';
-                break;
+	/* reverse does two things:
+	1. reverse the stream of characters
+	2. reverse each character to it's directional opposite.
+	This obviously won't work for asymetrical mud paths, but there's
+	not much we can do about that :P
+	*/
+	int POS1=0; /*position in stream of string of directions 
+	that have already been through the numbers() function */
+	int STRLENGTH=0; /*length of the string. */
+	char P; /* individual character to process */
+	char T; /* reversed character */
+	STRLENGTH=strlen(NEWSTR); /*get string length*/
+	for ( POS1=STRLENGTH; POS1 > 0; POS1-- ) /*create a loop starting at the end of the string and go backwards*/
+		{
+		switch (NEWSTR[POS1-1]) { /* reverse each directional character using a switch */
+	           case 'n':
+	                T='s';
+	                break;
+	           case 's':
+	                T='n';
+	                break;
+	           case 'w':
+	                T='e';
+	                break;
+	           case 'e':
+	                T='w';
+	                break;
+	           case 'u':
+	                T='d';
+	                break;
+	           case 'd':
+	                T='u';
+	                break;
+	           default: /* reset the character to null to avoid any possibility of unwanted characters */
+	                T='\0';
+	                break;
+			}
+		TMPSTR[STRLENGTH-POS1] = T; /*assign newly reversed direction to the end of the reversed string */
+	
 		}
-	TMPSTR[STRLENGTH-POS1] = T; /*assign newly reversed direction to the end of the reversed string */
-
-	}
 
 }
 
 
 int main(int argc, char *argv[]){
-
-/*print usuage information if user does not enter any arguments after the command*/
-if ( argc < 2 )
-	{
-	print_usage();
-	}
-
-/*if "-r" is found on the first argument, the reverse flag is set*/
-else {
-if ( (strcmp(argv[1],"-r")) == 0 ) {
-	strcpy(ENTSTR, argv[2]);
-	REVERSE=1;
-	}
-
-/*standard directional parsing if everything else fails*/
-else {	
-strcpy(ENTSTR, argv[1]);
-}
-
-/*main parsing loop.  here characters are singled out that match our criteria for digits or directions*/
-while( ENTSTR[POS] ){
-	C=ENTSTR[POS];
-	if ((C >= '0') && (C <= '9')) {
-		numbers();
+	
+	/*print usuage information if user does not enter any arguments after the command*/
+	if ( argc < 2 )
+		{
+		print_usage();
 		}
-	if ((C == 'n') || (C == 's') || (C == 'w') || (C == 'e') || (C == 'u') || (C == 'd')) {
-		directions();
+	
+	/*if "-r" is found on the first argument, the reverse flag is set*/
+	else {
+	if ( (strcmp(argv[1],"-r")) == 0 ) {
+		strcpy(ENTSTR, argv[2]);
+		REVERSE=1;
 		}
-	POS++;
-
-}
-
-/*after completing parsing operations, determine whether reverse mode is being requested, if not, print out
-directions and exit */
-/*printf("converted string is\n%s\n\n",NEWSTR);*/
-if ( REVERSE == 1 ) {
-	reverse();
-	printf("reversed %s\n", TMPSTR);
+	
+	/*standard directional parsing if everything else fails*/
+	else {	
+		strcpy(ENTSTR, argv[1]);
 	}
-else {
-	printf("%s\n",NEWSTR);
+	
+	/*main parsing loop.  here characters are singled out that match our criteria for digits or directions*/
+	while( ENTSTR[POS] ){
+		C=ENTSTR[POS];
+		if ((C >= '0') && (C <= '9')) {
+			numbers();
+			}
+		if ((C == 'n') || (C == 's') || (C == 'w') || (C == 'e') || (C == 'u') || (C == 'd')) {
+			directions();
+			}
+		POS++;
+	
 	}
-
-}
-return 0;
+	
+	/*after completing parsing operations, determine whether reverse mode is being requested, if not, print out
+	directions and exit */
+	/*printf("converted string is\n%s\n\n",NEWSTR);*/
+	if ( REVERSE == 1 ) {
+		reverse();
+		printf("reversed %s\n", TMPSTR);
+	}
+	else {
+		printf("%s\n",NEWSTR);
+	}
+	
+	}
+	return 0;
 }
